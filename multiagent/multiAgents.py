a96299832243b8fdc9063d7a36c3da600ac25e61
DayvanRider <hecobroe@gmx.de>
2021-10-11 11:11:55 +0200

    Beautified Code!

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -1,24 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # multiAgents.py
 # --------------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-
 from util import Queue, manhattanDistance
 from game import Directions
-import random, util
+import random
+import util
 
 from game import Agent
 
+
 class ReflexAgent(Agent):
+
     """
     A reflex agent chooses an action at each choice point by examining
     its alternatives via a state evaluation function.
@@ -27,8 +31,6 @@ class ReflexAgent(Agent):
     it in any way you see fit, so long as you don't touch our method
     headers.
     """
-
-    
 
     def getAction(self, gameState):
         """
@@ -41,15 +43,17 @@ class ReflexAgent(Agent):
         """
 
         # Collect legal moves and successor states
+
         legalMoves = gameState.getLegalActions()
 
         # Choose one of the best actions
-        scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
-        bestScore = max(scores)
-        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
 
-        "Add more of your code here if you want to"
+        scores = [self.evaluationFunction(gameState, action)
+                  for action in legalMoves]
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores))
+                       if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices)  # Pick randomly among the best
 
         return legalMoves[chosenIndex]
 
@@ -68,15 +72,19 @@ class ReflexAgent(Agent):
         Print out these variables to see what you're getting, then combine them
         to create a masterful evaluation function.
         """
+
         # Useful information you can extract from a GameState (pacman.py)
-        successorGameState = currentGameState.generatePacmanSuccessor(action)
+
+        successorGameState = \
+            currentGameState.generatePacmanSuccessor(action)
         newPos = successorGameState.getPacmanPosition()
         newFood = successorGameState.getFood()
         newGhostStates = successorGameState.getGhostStates()
-        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+        newScaredTimes = [ghostState.scaredTimer for ghostState in
+                          newGhostStates]
 
-        "*** YOUR CODE HERE ***"
         return successorGameState.getScore()
+
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -86,9 +94,12 @@ def scoreEvaluationFunction(currentGameState):
     This evaluation function is meant for use with adversarial search agents
     (not reflex agents).
     """
+
     return currentGameState.getScore()
 
+
 class MultiAgentSearchAgent(Agent):
+
     """
     This class provides some common elements to all of your
     multi-agent searchers.  Any methods defined here will be available
@@ -103,13 +114,23 @@ class MultiAgentSearchAgent(Agent):
     is another abstract class.
     """
 
-    def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
-        self.index = 0 # Pacman is always agent index 0
+    def __init__(self, evalFn='scoreEvaluationFunction', depth='2'):
+        self.index = 0  # Pacman is always agent index 0
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
 
-class Node :
-    def __init__(self, depth = 0, parent = 0, value = None, moove = None, gameState = None, children = []):
+
+class Node:
+
+    def __init__(
+        self,
+        depth=0,
+        parent=0,
+        value=None,
+        moove=None,
+        gameState=None,
+        children=[],
+        ):
         self.value = value
         self.depth = depth
         self.parent = parent
@@ -136,131 +157,116 @@ class Node :
         return self.value
 
 
-
 class MinimaxAgent(MultiAgentSearchAgent):
+
     """
     Your minimax agent (question 2)
     """
-    def evaluationFunction(self, currentGameState, action): 
+
+    def evaluationFunction(self, currentGameState, action):
 
         # Useful information you can extract from a GameState (pacman.py)
-        successorGameState = currentGameState.generatePacmanSuccessor(action)
-        newPos = successorGameState.getPacmanPosition()
-        newFood = successorGameState.getFood() ## Boolean grid
-        newGhostStates = successorGameState.getGhostStates()
-        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates] # Grid with the remaining moves being scared for the ghosts
-        
-        return successorGameState.getScore()
-    
-    def getAction(self, gameState):
-        
-        depth = 5
 
-        """ index = depth % gameState.getNumAgents() """
+        successorGameState = \
+            currentGameState.generatePacmanSuccessor(action)
+        newPos = successorGameState.getPacmanPosition()
+        newFood = successorGameState.getFood()  # # Boolean grid
+        newGhostStates = successorGameState.getGhostStates()
+        newScaredTimes = [ghostState.scaredTimer for ghostState in
+                          newGhostStates]  # Grid with the remaining moves being scared for the ghosts
+
+        return successorGameState.getScore()
+
+    def getAction(self, gameState):
+
+        depth = 5
 
         queue = Queue()
 
         # TODO : Work in progress name
+
         otherQueue = Queue()
 
-        firstNode = Node(depth=0, gameState= gameState)
+        firstNode = Node(depth=0, gameState=gameState, value =float('-inf'))
         queue.push(firstNode)
 
-        for i in range (depth) :
+        for i in range(depth):
 
             index = i % gameState.getNumAgents()
-            if(queue.isEmpty()) :
-                print("TRY")
+            if queue.isEmpty():
+                print ('TRY')
                 continue
-            else :
+            else:
                 element = queue.pop()
 
             # Collect legal moves from the element removed from the queue
+
             legalPacManMoves = element.gameState.getLegalActions(0)
-            
-            for e in legalPacManMoves :
 
-                node = Node(depth= depth-i, parent= element, moove = e, gameState= gameState.generateSuccessor(index, e))
+            for e in legalPacManMoves:
 
-                if(index != 0) :
+                node = Node(depth=depth - i, parent=element, moove=e,
+                            gameState=gameState.generateSuccessor(index,
+                            e))
+
+                if index != 0:
                     node.set_value(float('inf'))
-                else :
+                else:
                     node.set_value(float('-inf'))
 
-                if(node.gameState.isWin()):
+                if node.gameState.isWin():
                     node.set_value(float('inf'))
                     otherQueue.push(node)
+                elif node.gameState.isLose():
 
-                elif(node.gameState.isLose()):
                     node.set_value(float('-inf'))
                     otherQueue.push(node)
+                elif i == depth - 1:
 
-                elif(i == depth-1):
-                    node.set_value(self.evaluationFunction(node.gameState, e))
+                    node.set_value(self.evaluationFunction(node.gameState,
+                                   e))
                     otherQueue.push(node)
+                else:
 
-                else :
                     queue.push(node)
 
                 # Add to element the new child created
+
                 element.children.append(node)
 
-        max = Node(depth = -1, value= float('-inf'))
+        max = Node(depth=-1, value=float('-inf'))
 
-        while (not otherQueue.isEmpty()):
-            
+        while not otherQueue.isEmpty():
+
             node = otherQueue.pop()
-			
+
             if node == firstNode:
                 continue
-			
+
             parent = node.parent
 
             index = parent.depth % parent.gameState.getNumAgents()
 
-            if(index == 0 and parent.get_value() < node.get_value()):
+            if index == 0 and parent.get_value() < node.get_value():
                 parent.set_value(node.get_value())
-            
-            elif(index != 0 and parent.get_value() > node.get_value()):
+            elif index != 0 and parent.get_value() > node.get_value():
+
                 parent.set_value(node.get_value())
 
-            if(parent not in otherQueue):
+            if parent not in otherQueue:
                 otherQueue.push(parent)
 
-		
-		for e in firstNode.children:
-			if (e.get_value() > max.get_value()):
-				max = e
-        
+        for e in firstNode.children:
+            if e.get_value() > max.get_value():
+                max = e
+
         return max.moove
 
-            
-        """
-        Returns the minimax action from the current gameState using self.depth
-        and self.evaluationFunction.
-
-        Here are some method calls that might be useful when implementing minimax.
-
-        gameState.getLegalActions(agentIndex):
-        Returns a list of legal actions for an agent
-        agentIndex=0 means Pacman, ghosts are >= 1
-
-        gameState.generateSuccessor(agentIndex, action):
-        Returns the successor game state after an agent takes an action
-
-        gameState.getNumAgents():
-        Returns the total number of agents in the game
-
-        gameState.isWin():
-        Returns whether or not the game state is a winning state
-
-        gameState.isLose():
-        Returns whether or not the game state is a losing state
-        """
-        "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
 
+
 class AlphaBetaAgent(MultiAgentSearchAgent):
+
     """
     Your minimax agent with alpha-beta pruning (question 3)
     """
@@ -269,10 +275,12 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         """
         Returns the minimax action using self.depth and self.evaluationFunction
         """
-        "*** YOUR CODE HERE ***"
+
         util.raiseNotDefined()
 
+
 class ExpectimaxAgent(MultiAgentSearchAgent):
+
     """
       Your expectimax agent (question 4)
     """
@@ -284,8 +292,9 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         All ghosts should be modeled as choosing uniformly at random from their
         legal moves.
         """
-        "*** YOUR CODE HERE ***"
+
         util.raiseNotDefined()
+
 
 def betterEvaluationFunction(currentGameState):
     """
@@ -294,8 +303,10 @@ def betterEvaluationFunction(currentGameState):
 
     DESCRIPTION: <write something here so we know what you did>
     """
-    "*** YOUR CODE HERE ***"
+
     util.raiseNotDefined()
 
+
 # Abbreviation
+
 better = betterEvaluationFunction
